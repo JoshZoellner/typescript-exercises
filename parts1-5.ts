400cc9918fe30c35f02387558d83861a1849a05a
Josh Zoellner <Josh.Zoellner@gmail.com>
2020-05-20 22:24:53 -0400

    added code to print outputs for trips to Mars and the Moon using the new function

--- parts1-5.ts
+++ parts1-5.ts
@@ -31,6 +31,8 @@ function getDaysToLocation(kilometersAway: number, speed: number): number{
 
 // Move your output statement from part 2 here. Update the template literal to call
 // the function and print the outputs for a Mars trip and a moon trip.
+console.log(`${spacecraftName} would take ${getDaysToLocation(kilometersToMars, speedMph)} days to get to Mars.`);
+console.log(`${spacecraftName} would take ${getDaysToLocation(kilometersToTheMoon, speedMph)} days to get to the Moon.`);
 
 // Part 4: Create a Spacecraft Class
 
